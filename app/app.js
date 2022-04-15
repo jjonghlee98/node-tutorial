12956f917992efdfabbd3c2fa97d7c9335b09be6
Jonghwan Lee <jjonghlee98@gmail.com>
2022-04-15 18:09:24 +0900

    nodejs start tutorial

---
+++ app/app.js
@@ -0,0 +1,12 @@
+"use strict";
+const express = require('express');
+const app = express();
+
+const router = require('./routes/home');
+
+app.set('views', 'views');
+app.set('view engine', 'ejs');
+
+app.use('/', router);
+
+module.exports = app;
